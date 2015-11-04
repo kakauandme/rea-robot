9a707893d581ddc66220faa667d3d2c58f9bb948
Kirill Kliavin <k.kliavin@gmail.com>
2015-11-04 23:30:13 +1100

    comments

--- includes/robot.js
+++ includes/robot.js
@@ -1,26 +1,32 @@
+//constructor (String, Integer, Integer)
 var Robot = function(_name, _width, _height) {
 
+	//set robot's name
 	this.name = _name;
 
 	if(_width > 0 && _height > 0){
+
+		//set table dimensions
 		this.table_width = _width; 
-  		this.table_height = _height;
+		this.table_height = _height;
 	}else{
 		throw new Error('Table width & height should be positive integers');
 	}
-  	
-  	this.directions = ["NORTH", "EAST", "SOUTH", "WEST"];
+	
+	this.directions = ["NORTH", "EAST", "SOUTH", "WEST"];
 
-  	this.x = false;
-  	this.y = false;
-  	this.facing = false;
+	this.x = false;
+	this.y = false;
+	this.facing = false;
 
 };
 
+//checks if robot is placed
 Robot.prototype.isPlaced = function() {
 	return (this.x !== false && this.y !== false && this.facing !== false);
 };
 
+//place (Integer, Integer, String)
 Robot.prototype.place = function(_x,_y,_facing) {
 
 	if(_x >= 0 && _x < this.table_width && _y >= 0 && _y < this.table_height){
@@ -28,13 +34,16 @@ Robot.prototype.place = function(_x,_y,_facing) {
 		this.y = _y;
 	}else{
 		this.x = false;
-  		this.y = false;
+		this.y = false;
 	}
 
+	//loop through all possible directions
 	var i = 0;
 	for (;i < this.directions.length; i++) {
-	
+		
 		if(this.directions[i] == _facing.toUpperCase()){
+
+			//assign if it matches
 			this.facing = i;
 			break;
 		}
@@ -43,6 +52,8 @@ Robot.prototype.place = function(_x,_y,_facing) {
 		this.facing = false;
 	}
 };
+
+//print curent position if placed
 Robot.prototype.report = function() {
 
 	if(this.isPlaced()){
@@ -51,56 +62,60 @@ Robot.prototype.report = function() {
 	}
 };
 
+
+//move, unless on the edge or not placed
 Robot.prototype.move = function() {
 	if(this.isPlaced()){
 
 		switch (this.directions[this.facing]){
 			case "NORTH":
-				if ((this.y+1) < this.table_height){
-					this.y++;
-				}
-				break;
+			if ((this.y+1) < this.table_height){
+				this.y++;
+			}
+			break;
 			case "SOUTH":
-				if ((this.y-1) >= 0){
-					this.y--;
-				}
-				break;
+			if ((this.y-1) >= 0){
+				this.y--;
+			}
+			break;
 			case "EAST":
-				if ((this.x+1) < this.table_width){
-					this.x++;
-				}
-				break;
+			if ((this.x+1) < this.table_width){
+				this.x++;
+			}
+			break;
 			case "WEST":
-				if ((this.x-1) >= 0){
-					this.x--;
-				}
+			if ((this.x-1) >= 0){
+				this.x--;
+			}
 		}
 	}
 };
 
-
+//change direction counter clockwise
 Robot.prototype.left = function() {
 	if(this.isPlaced()){
 		if (this.facing===0){
 			this.facing = this.directions.length-1;
 		}else{
 			this.facing--;
-		
+			
 		}
 	}
 };
 
+//change direction clockwise
 Robot.prototype.right = function() {
 	if(this.isPlaced()){
 		if (this.facing === this.directions.length-1){
 			this.facing = 0;
 		}else{
 			this.facing++;
-		
+			
 		}
 	}
 };
 
+//process commands (String)
 Robot.prototype.listen = function(_input){
 	if(_input === "REPORT"){
 		this.report();
@@ -111,6 +126,7 @@ Robot.prototype.listen = function(_input){
 	}else if(_input === "RIGHT"){
 		this.right();
 	}else {
+		//match placing format
 		var pattern = /^PLACE\s(\d),\s?(\d),\s?([a-zA-Z]{4,5})$/i;
 		if(pattern.test(_input)){
 			var args = _input.match(pattern)
@@ -119,7 +135,7 @@ Robot.prototype.listen = function(_input){
 	}
 }
 
-
+//print instructions
 Robot.prototype.welcome= function() {
 	console.log("\nHi human! I'm "+this.name+" and you can conrol me with several commands. \nBy typing PLACE X,Y, DIRECTION where X & Y are cordinates on a "+this.table_width+" by "+this.table_height+" table I will be placed on and DIRECTION is cardinal direction I will be facing. \nType MOVE and I'll go one step forward. \nLEFT & RIGHT will change my facing direction accordignly.\nFinnaly, REPORT will print my current location. \nOh, almost forget, if you are done - just type EXIT. \nEnjoy!\n");
 };
--- main.js
+++ main.js
@@ -5,6 +5,8 @@ var robot = new Robot("Reabot", 5 , 5);
 
 robot.welcome();
 
+
+//read test file
 if(process.argv.length > 2){
 	var rl = require('readline').createInterface({
 	  input: require('fs').createReadStream(process.argv[2])
@@ -15,6 +17,8 @@ if(process.argv.length > 2){
 	});
 }
 
+
+//get input from STD and pass it to robot or exit
 process.stdin.resume();
 process.stdin.setEncoding('utf8');
 process.stdin.on('data', function (text) {
