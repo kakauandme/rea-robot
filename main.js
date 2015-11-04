9fb42102e83020eb243f9d28054e18cff2848a82
Kirill Kliavin <k.kliavin@gmail.com>
2015-11-04 22:27:41 +1100

    read stdin

--- main.js
+++ main.js
@@ -3,9 +3,16 @@ var Robot = require('./includes/robot');
 
 var robot = new Robot("Reabot", 5 , 5);
 robot.welcome();
-robot.place(0,0,"north");
-robot.report();
-robot.move();
-robot.right();
-robot.move();
-robot.report();
+
+
+
+
+process.stdin.resume();
+process.stdin.setEncoding('utf8');
+process.stdin.on('data', function (text) {
+	if ((text= text.trim().toUpperCase()) === 'EXIT') {
+	 	process.exit();
+	}else{
+		robot.listen(text);
+	}	
+});
